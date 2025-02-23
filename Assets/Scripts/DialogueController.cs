eec8c5e09d264e8ba4d6447399875fba1bf305c7
jazzydingo <129543893+jazzydingo@users.noreply.github.com>
2025-02-22 23:36:27 -0700

    TMP and basic dialogue system
    set up text mesh pro, set up basic dialogue system

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class DialogueController : MonoBehaviour
+{
+    public TextMeshProUGUI dialogue;
+    public bool skip;
+    public string[] dialogueLines;
+    public int index;
+    public float textSpeed;
+    public bool endOfDialogue;
+    public GameObject dialogueObj;
+    public bool isPlaying;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        skip = false;
+        dialogue.text = string.Empty;
+        dialogueObj.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !endOfDialogue && !isPlaying)
+        {
+
+            dialogueObj.gameObject.SetActive(true);
+            skip = true;
+            StartCoroutine(NextLine());
+        }
+        else if(Input.GetMouseButtonDown(0) && !endOfDialogue && isPlaying)
+        {
+            skip = true;
+        }
+        else if (Input.GetMouseButtonDown(0) && endOfDialogue && !isPlaying)
+        {
+            dialogueObj.gameObject.SetActive(false);
+        }
+        else
+        {
+
+        }
+    }
+
+    IEnumerator NextLine()
+    {
+        if (!endOfDialogue)
+        {
+            dialogue.text = string.Empty;
+            skip = false;
+            isPlaying = true;
+
+            foreach (char letter in dialogueLines[index])
+            {
+                dialogue.text += letter;
+
+                if (!skip)
+                {
+                    yield return new WaitForSeconds(textSpeed);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            isPlaying = false;
+            dialogue.text = dialogueLines[index];
+            if (index < dialogueLines.Length - 1)
+            {
+                index++;
+                skip = false;
+                NextLine();
+            }
+            else
+            {
+                skip = false;
+                endOfDialogue = true;
+                yield return null;
+            }
+        }
+
+    }
+}
