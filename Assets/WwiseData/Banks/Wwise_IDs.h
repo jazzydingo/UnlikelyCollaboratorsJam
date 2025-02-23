76a4cb8488c7dd415262d1318fbd907d356aa49d
Gorkovoy1 <101889868+Gorkovoy1@users.noreply.github.com>
2025-02-22 22:11:26 -0700

    WWISE WEBGL TEST SCENE

--- Assets/WwiseData/Banks/Wwise_IDs.h
+++ Assets/WwiseData/Banks/Wwise_IDs.h
@@ -11,6 +11,11 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID TEST = 3157003241U;
+    } // namespace EVENTS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
